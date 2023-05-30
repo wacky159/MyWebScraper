2bf6d39b58ae1b7f6981c4b9cd1cf60ca02bc6ee
henry <wacky159@gmail.com>
2023-05-30 17:04:45 +0800

    fix: 修復再次點擊出現links重複宣告錯誤

--- content.js
+++ content.js
@@ -1,2 +1,8 @@
-let links = [...document.getElementsByTagName("a")].map((link) => link.href);
+if (typeof links === undefined) {
+  let links = [...document.getElementsByTagName("a")].map((link) => link.href);
+} else {
+  links = [...document.getElementsByTagName("a")].map(
+    (link) => link.href
+  );
+}
 console.log(links);
